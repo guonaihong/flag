faff58a702df1b66f15ed2b02069366b55eb9992
guonaihong <guonaihong@qq.com>
2019-06-04 20:11:52 +0800

    自动插入-h, --help, -V, --version选项 和struct功能的一些测试代码
    updates guonaihong/flag#2
    updates guonaihong/flag#5

--- flag.go
+++ flag.go
@@ -65,7 +65,6 @@
 package flag
 
 import (
-	"bytes"
 	"errors"
 	"fmt"
 	"io"
@@ -73,7 +72,6 @@ import (
 	"reflect"
 	"regexp"
 	"sort"
-	"strconv"
 	"strings"
 	"time"
 )
@@ -81,210 +79,7 @@ import (
 // ErrHelp is the error returned if the -help or -h flag is invoked
 // but no such flag is defined.
 var ErrHelp = errors.New("flag: help requested")
-
-// -- bool Value
-type boolValue bool
-
-func newBoolValue(val bool, p *bool) *boolValue {
-	*p = val
-	return (*boolValue)(p)
-}
-
-func (b *boolValue) Set(s string) error {
-	v, err := strconv.ParseBool(s)
-	*b = boolValue(v)
-	return err
-}
-
-func (b *boolValue) Get() interface{} { return bool(*b) }
-
-func (b *boolValue) String() string { return strconv.FormatBool(bool(*b)) }
-
-func (b *boolValue) IsBoolFlag() bool { return true }
-
-// optional interface to indicate boolean flags that can be
-// supplied without "=value" text
-type boolFlag interface {
-	Value
-	IsBoolFlag() bool
-}
-
-// -- int Value
-type intValue int
-
-func newIntValue(val int, p *int) *intValue {
-	*p = val
-	return (*intValue)(p)
-}
-
-func (i *intValue) Set(s string) error {
-	v, err := strconv.ParseInt(s, 0, strconv.IntSize)
-	*i = intValue(v)
-	return err
-}
-
-func (i *intValue) Get() interface{} { return int(*i) }
-
-func (i *intValue) String() string { return strconv.Itoa(int(*i)) }
-
-// -- int64 Value
-type int64Value int64
-
-func newInt64Value(val int64, p *int64) *int64Value {
-	*p = val
-	return (*int64Value)(p)
-}
-
-func (i *int64Value) Set(s string) error {
-	v, err := strconv.ParseInt(s, 0, 64)
-	*i = int64Value(v)
-	return err
-}
-
-func (i *int64Value) Get() interface{} { return int64(*i) }
-
-func (i *int64Value) String() string { return strconv.FormatInt(int64(*i), 10) }
-
-// -- uint Value
-type uintValue uint
-
-func newUintValue(val uint, p *uint) *uintValue {
-	*p = val
-	return (*uintValue)(p)
-}
-
-func (i *uintValue) Set(s string) error {
-	v, err := strconv.ParseUint(s, 0, strconv.IntSize)
-	*i = uintValue(v)
-	return err
-}
-
-func (i *uintValue) Get() interface{} { return uint(*i) }
-
-func (i *uintValue) String() string { return strconv.FormatUint(uint64(*i), 10) }
-
-// -- uint64 Value
-type uint64Value uint64
-
-func newUint64Value(val uint64, p *uint64) *uint64Value {
-	*p = val
-	return (*uint64Value)(p)
-}
-
-func (i *uint64Value) Set(s string) error {
-	v, err := strconv.ParseUint(s, 0, 64)
-	*i = uint64Value(v)
-	return err
-}
-
-func (i *uint64Value) Get() interface{} { return uint64(*i) }
-
-func (i *uint64Value) String() string { return strconv.FormatUint(uint64(*i), 10) }
-
-// -- string Value
-type stringValue string
-
-func newStringValue(val string, p *string) *stringValue {
-	*p = val
-	return (*stringValue)(p)
-}
-
-func (s *stringValue) Set(val string) error {
-	*s = stringValue(val)
-	return nil
-}
-
-func (s *stringValue) Get() interface{} { return string(*s) }
-
-func (s *stringValue) String() string { return string(*s) }
-
-// -- float64 Value
-type float64Value float64
-
-func newFloat64Value(val float64, p *float64) *float64Value {
-	*p = val
-	return (*float64Value)(p)
-}
-
-func (f *float64Value) Set(s string) error {
-	v, err := strconv.ParseFloat(s, 64)
-	*f = float64Value(v)
-	return err
-}
-
-func (f *float64Value) Get() interface{} { return float64(*f) }
-
-func (f *float64Value) String() string { return strconv.FormatFloat(float64(*f), 'g', -1, 64) }
-
-// -- time.Duration Value
-type durationValue time.Duration
-
-func newDurationValue(val time.Duration, p *time.Duration) *durationValue {
-	*p = val
-	return (*durationValue)(p)
-}
-
-func (d *durationValue) Set(s string) error {
-	v, err := time.ParseDuration(s)
-	*d = durationValue(v)
-	return err
-}
-
-func (d *durationValue) Get() interface{} { return time.Duration(*d) }
-
-func (d *durationValue) String() string { return (*time.Duration)(d).String() }
-
-// -- duration slice value
-type durationSliceValue []time.Duration
-
-func newDurationSliceValue(val []time.Duration, p *[]time.Duration) *durationSliceValue {
-	*p = val
-	return (*durationSliceValue)(p)
-}
-
-func (d *durationSliceValue) Set(val string) error {
-	var dv durationValue
-
-	err := dv.Set(val)
-	if err != nil {
-		return err
-	}
-
-	*d = append(*d, time.Duration(dv))
-	return nil
-}
-
-func (d *durationSliceValue) Get() interface{} {
-	return []time.Duration(*d)
-}
-
-func (d *durationSliceValue) String() string {
-	switch len(*d) {
-	case 0:
-		return "[]"
-	case 1:
-		return fmt.Sprintf("[%s]", (*d)[0])
-	case 2:
-		return fmt.Sprintf("[%s, %s]", (*d)[0], (*d)[1])
-	case 3:
-		return fmt.Sprintf("[%s, %s, %s]", (*d)[0], (*d)[1], (*d)[2])
-	}
-
-	var buf bytes.Buffer
-
-	buf.WriteString("[")
-
-	for k, v := range *d {
-		buf.WriteString((*durationValue)(&v).String())
-		if k != len(*d)-1 {
-			buf.WriteString(",")
-		}
-	}
-
-	buf.WriteString("]")
-
-	return buf.String()
-}
+var ErrVersion = errors.New("flag: version requested")
 
 // Value is the interface to the dynamic value stored in a flag.
 // (The default value is represented as a string.)
@@ -596,16 +391,17 @@ func PrintDefaults() {
 	CommandLine.PrintDefaults()
 }
 
-func (f *FlagSet) printVersionAuthor() {
-	if f.version != "" {
-		name := f.name
-		if name != "" {
-			name += " "
-		}
-
-		fmt.Fprintf(f.Output(), "%s%s\n", name, f.version)
+func (f *FlagSet) printVersion() {
+	name := f.name
+	if name != "" {
+		name += " "
 	}
 
+	fmt.Fprintf(f.Output(), "%s%s\n", name, f.version)
+
+}
+
+func (f *FlagSet) printVersionAuthor() {
 	if f.author != "" {
 		fmt.Fprintf(f.Output(), "%s\n\n", f.author)
 	}
@@ -945,6 +741,10 @@ func initFormal(formal *map[string]*Flag) {
 }
 
 func (f *FlagSet) alreadythereError(name string) {
+	switch name {
+	case "h", "help", "V", "version":
+		return
+	}
 	var msg string
 	if f.name == "" {
 		msg = fmt.Sprintf("flag redefined: %s", name)
@@ -1032,6 +832,16 @@ func (f *FlagSet) getFlag(name string) (*Flag, bool, error) {
 	formals := make([]map[string]*Flag, 0, 4)
 	formals = append(formals, f.formal, f.shortLong, f.regex)
 
+	if name == "help" || name == "h" { // special case for nice help message.
+		f.usage()
+		return nil, false, ErrHelp
+	}
+
+	if name == "version" || name == "V" {
+		f.printVersion()
+		return nil, false, ErrVersion
+	}
+
 	for k, formal := range formals {
 		if k == len(formals)-1 && len(formal) > 0 { //range regexp map
 			for k, v := range formal {
@@ -1050,11 +860,6 @@ func (f *FlagSet) getFlag(name string) (*Flag, bool, error) {
 		}
 
 		return flag, true, nil
-	}
-
-	if name == "help" || name == "h" { // special case for nice help message.
-		f.usage()
-		return nil, false, ErrHelp
 	}
 
 	return nil, false, fmt.Errorf("flag provided but not defined: -%s", name)
@@ -1264,6 +1069,13 @@ try:
 		hasValue = true
 	}
 	return f.setFlag(flag, name, hasValue, value)
+
+}
+
+// Auto-generated Help, Version, and Usage information
+func (f *FlagSet) generatedOpt() {
+	f.Bool("h, help", false, "display this help and exit")
+	f.Bool("V, version", false, "output version information and exit")
 }
 
 // Parse parses flag definitions from the argument list, which should not
@@ -1271,6 +1083,7 @@ try:
 // are defined and before flags are accessed by the program.
 // The return value will be ErrHelp if -help or -h were set but not defined.
 func (f *FlagSet) Parse(arguments []string) error {
+
 	f.parsed = true
 	f.args = arguments
 
@@ -1338,23 +1151,24 @@ func NewFlagSet(name string, errorHandling ErrorHandling) *FlagSet {
 		errorHandling: errorHandling,
 	}
 	f.Usage = f.defaultUsage
+	f.generatedOpt()
 	return f
 }
 
 //
 //
-func (f *FlagSet) Version(version string) {
+func (f *FlagSet) Version(version string) *FlagSet {
 	f.version = version
+	return f
 }
 
-//
-//
+func (f *FlagSet) Author(author string) *FlagSet {
+	f.author = author
+	return f
+}
+
 func Version(version string) {
 	CommandLine.Version(version)
-}
-
-func (f *FlagSet) Author(author string) {
-	f.author = author
 }
 
 func Author(author string) {
